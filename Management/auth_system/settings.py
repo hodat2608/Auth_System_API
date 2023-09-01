42b8b9db20de8410b604e2defec1af498e9db0bd
hodat <hodat@gmail.com>
2023-09-02 00:05:13 +0700

    1...

--- Management/auth_system/settings.py
+++ Management/auth_system/settings.py
@@ -174,6 +174,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+from datetime import timedelta
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
